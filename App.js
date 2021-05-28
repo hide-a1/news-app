5d9d34cf35f0c317a84429590462c8a46100b72b
hide-a1 <hide210g@gmail.com>
2021-05-28 11:33:25 +0900

    feat: store

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import AppNavigator from './navigation/AppNavigator';
+import { Provider } from 'react-redux';
+import store from './store';
 
 export default function App() {
   const Stack = createStackNavigator();
-  return <AppNavigator />;
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
 }
