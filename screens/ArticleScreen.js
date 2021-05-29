bbe18ebae9dd2d9f13d312b5109f418355e2fd17
hide-a1 <hide210g@gmail.com>
2021-05-29 10:23:11 +0900

    feat: tabBar

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { useDispatch } from 'react-redux';
+import { addClip } from '../store/actions/user';
+import { deleteClip } from '../store/actions/user';
 
 export default ArticleScreen = ({ route }) => {
   const { article } = route.params;
-  console.log(article);
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(addClip({ clip: article }));
+        }}
+      >
+        <Text style={{ margin: 10, fontSize: 30 }}>ADD_CLIP</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(deleteClip({ clip: article }));
+        }}
+      >
+        <Text style={{ margin: 10, fontSize: 30 }}>DELETE_CLIP</Text>
+      </TouchableOpacity>
       <WebView source={{ uri: article.url }} />
     </SafeAreaView>
   );
